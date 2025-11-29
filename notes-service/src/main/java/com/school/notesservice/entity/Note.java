6f95a75b4203b2c82658efab20439ab9816cae35
Chayma Chliyah <chliyahchaimae2003@gmail.com>
2025-11-29 23:29:18 +0100

    Note repo & entity

---
+++ notes-service/src/main/java/com/school/notesservice/entity/Note.java
@@ -0,0 +1,60 @@
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Note{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long etudiantId;
+    private String module;
+    private double valeur;
+    private String session;
+
+    public Note() {
+    }
+    public Note(Long etudiantId, String module, double valeur, String session) {
+        this.etudiantId = etudiantId;
+        this.module = module;
+        this.valeur = valeur;
+        this.session = session;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getEtudiantId() {
+        return etudiantId;
+    }
+
+    public void setEtudiantId(Long etudiantId) {
+        this.etudiantId = etudiantId;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public double getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(double valeur) {
+        this.valeur = valeur;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+}
--- notes-service/src/main/java/com/school/notesservice/repository/NoteRepository.java
+++ notes-service/src/main/java/com/school/notesservice/repository/NoteRepository.java
@@ -1,5 +1,8 @@
+package com.school.noteservice.repository;
 
-
-interface  implements JpaRepository {
-
+import com.school.noteservice.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByEtudiantId(Long etudiantId);
 }
